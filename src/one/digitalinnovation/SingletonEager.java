ea6a6a2d3937a01a165a7afc80b37bc5837c22ea
Rudy Fernandez <rudyfernandez2002@gmail.com>
2024-07-23 13:35:10 -0300

    lab-padroes-projeto-java

---
+++ src/one/digitalinnovation/SingletonEager.java
@@ -0,0 +1,11 @@
+package one.digitalinnovation;
+
+public class SingletonEager {
+    private static SingletonEager instancia = new SingletonEager();
+    private SingletonEager() {
+        super();
+    }
+    public static SingletonEager getInstancia() {
+        return instancia;
+    }
+}
---
+++ src/one/digitalinnovation/SingletonLazy.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation;
+
+public class SingletonLazy {
+    private static SingletonLazy instancia;
+    private SingletonLazy() {
+        super();
+    }
+    public static  SingletonLazy getInstancia() {
+        if(instancia == null)
+            instancia = new SingletonLazy();
+        return instancia;
+    }
+}
